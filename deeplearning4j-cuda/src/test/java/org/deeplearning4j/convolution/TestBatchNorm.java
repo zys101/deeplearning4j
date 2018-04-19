fb4f0568570d56f808a32ff2333ba8e11768f90b
Alex Black <blacka101@gmail.com>
2018-04-19 17:34:42 +1000

    Make test more robust

--- deeplearning4j-cuda/src/test/java/org/deeplearning4j/convolution/TestBatchNorm.java
+++ deeplearning4j-cuda/src/test/java/org/deeplearning4j/convolution/TestBatchNorm.java
@@ -16,6 +16,7 @@ import org.junit.Test;
 import org.nd4j.linalg.activations.Activation;
 import org.nd4j.linalg.api.buffer.DataBuffer;
 import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.dataset.DataSet;
 import org.nd4j.linalg.factory.Nd4j;
 
 import java.lang.reflect.Field;
@@ -28,48 +29,75 @@ public class TestBatchNorm {
     public void testCompareMLN() throws Exception {
 //        Nd4j.setDataType(DataBuffer.Type.FLOAT);
 
-        MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
-                .seed(12345)
-                .activation(Activation.TANH)
-                .weightInit(WeightInit.XAVIER)
-                .convolutionMode(ConvolutionMode.Same)
-                .list()
-                .layer(new ConvolutionLayer.Builder().nIn(3).nOut(3).build())
-                .layer(new BatchNormalization.Builder().build())
-                .layer(new ConvolutionLayer.Builder().nIn(3).nOut(3).build())
-                .layer(new OutputLayer.Builder().nOut(10).build())
-                .setInputType(InputType.convolutional(10, 10, 3))
-                .build();
+        for( int minibatch : new int[]{1, 5}) {
 
-        MultiLayerNetwork net1 = new MultiLayerNetwork(conf);
-        net1.init();
+            MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
+                    .seed(12345)
+                    .activation(Activation.TANH)
+                    .weightInit(WeightInit.XAVIER)
+                    .convolutionMode(ConvolutionMode.Same)
+                    .list()
+                    .layer(new ConvolutionLayer.Builder().nIn(3).nOut(3).build())
+                    .layer(new BatchNormalization.Builder().build())
+                    .layer(new ConvolutionLayer.Builder().nIn(3).nOut(3).build())
+                    .layer(new OutputLayer.Builder().nOut(10).build())
+                    .setInputType(InputType.convolutional(10, 10, 3))
+                    .build();
 
-        MultiLayerNetwork net2 = new MultiLayerNetwork(conf.clone());
-        net2.init();
+            MultiLayerNetwork net1 = new MultiLayerNetwork(conf);
+            net1.init();
 
-        INDArray p1 = net1.params();
-        p1.assign(Nd4j.rand(1, p1.length()));
+            MultiLayerNetwork net2 = new MultiLayerNetwork(conf.clone());
+            net2.init();
 
-        net2.params().assign(p1);
+            INDArray p1 = net1.params();
+            p1.assign(Nd4j.rand(1, p1.length()));
 
-        Field f = org.deeplearning4j.nn.layers.normalization.BatchNormalization.class.getDeclaredField("helper");
-        f.setAccessible(true);
+            net2.params().assign(p1);
 
-        assertNotNull(f.get(net1.getLayer(1)));
-        assertTrue(f.get(net1.getLayer(1)) instanceof CudnnBatchNormalizationHelper);
+            Field f = org.deeplearning4j.nn.layers.normalization.BatchNormalization.class.getDeclaredField("helper");
+            f.setAccessible(true);
 
-        f.set(net2.getLayer(1), null);
-        assertNull(f.get(net2.getLayer(1)));
+            assertNotNull(f.get(net1.getLayer(1)));
+            assertTrue(f.get(net1.getLayer(1)) instanceof CudnnBatchNormalizationHelper);
 
-        INDArray in = Nd4j.rand(new int[]{2, 3, 10, 10});
-        INDArray out1Train = net1.output(in, true);
-        INDArray out2Train = net2.output(in, true);
+            f.set(net2.getLayer(1), null);
+            assertNull(f.get(net2.getLayer(1)));
 
-        INDArray out1Test = net1.output(in, false);
-        INDArray out2Test = net1.output(in, false);
+            Field f2 = org.deeplearning4j.nn.layers.convolution.ConvolutionLayer.class.getDeclaredField("helper");
+            f2.setAccessible(true);
+            f2.set(net2.getLayer(0), null);
+            f2.set(net2.getLayer(2), null);
 
-        assertEquals(out1Train, out2Train);
-        assertEquals(out1Test, out2Test);
+
+            INDArray in = Nd4j.rand(new int[]{minibatch, 3, 10, 10});
+            INDArray out1Train = net1.output(in, true);
+            INDArray out2Train = net2.output(in, true);
+
+            INDArray out1Test = net1.output(in, false);
+            INDArray out2Test = net1.output(in, false);
+
+            assertEquals(out1Train, out2Train);
+            assertEquals(out1Test, out2Test);
+
+
+            for (int i = 0; i < 10; i++) {
+                INDArray input = Nd4j.rand(new int[]{minibatch, 3, 10, 10});
+                INDArray labels = Nd4j.rand(minibatch, 10);
+                net1.fit(input, labels);
+                net2.fit(input, labels);
+            }
+
+            in = Nd4j.rand(new int[]{minibatch, 3, 10, 10});
+            out1Train = net1.output(in, true);
+            out2Train = net2.output(in, true);
+
+            out1Test = net1.output(in, false);
+            out2Test = net1.output(in, false);
+
+            assertEquals(out1Train, out2Train);
+            assertEquals(out1Test, out2Test);
+        }
     }
 
 
@@ -77,50 +105,75 @@ public class TestBatchNorm {
     public void testCompareCG() throws Exception {
 //        Nd4j.setDataType(DataBuffer.Type.FLOAT);
 
-        ComputationGraphConfiguration conf = new NeuralNetConfiguration.Builder()
-                .seed(12345)
-                .activation(Activation.TANH)
-                .weightInit(WeightInit.XAVIER)
-                .convolutionMode(ConvolutionMode.Same)
-                .graphBuilder()
-                .addInputs("in")
-                .layer("0", new ConvolutionLayer.Builder().nIn(3).nOut(3).build(), "in")
-                .layer("1", new BatchNormalization.Builder().build(), "0")
-                .layer("2", new ConvolutionLayer.Builder().nIn(3).nOut(3).build(), "1")
-                .layer("3", new OutputLayer.Builder().nOut(10).build(), "2")
-                .setOutputs("3")
-                .setInputTypes(InputType.convolutional(10, 10, 3))
-                .build();
+        for( int minibatch : new int[]{1, 5}) {
 
-        ComputationGraph net1 = new ComputationGraph(conf);
-        net1.init();
+            ComputationGraphConfiguration conf = new NeuralNetConfiguration.Builder()
+                    .seed(12345)
+                    .activation(Activation.TANH)
+                    .weightInit(WeightInit.XAVIER)
+                    .convolutionMode(ConvolutionMode.Same)
+                    .graphBuilder()
+                    .addInputs("in")
+                    .layer("0", new ConvolutionLayer.Builder().nIn(3).nOut(3).build(), "in")
+                    .layer("1", new BatchNormalization.Builder().build(), "0")
+                    .layer("2", new ConvolutionLayer.Builder().nIn(3).nOut(3).build(), "1")
+                    .layer("3", new OutputLayer.Builder().nOut(10).build(), "2")
+                    .setOutputs("3")
+                    .setInputTypes(InputType.convolutional(10, 10, 3))
+                    .build();
 
-        ComputationGraph net2 = new ComputationGraph(conf.clone());
-        net2.init();
+            ComputationGraph net1 = new ComputationGraph(conf);
+            net1.init();
 
-        INDArray p1 = net1.params();
-        p1.assign(Nd4j.rand(1, p1.length()));
+            ComputationGraph net2 = new ComputationGraph(conf.clone());
+            net2.init();
 
-        net2.params().assign(p1);
+            INDArray p1 = net1.params();
+            p1.assign(Nd4j.rand(1, p1.length()));
 
-        Field f = org.deeplearning4j.nn.layers.normalization.BatchNormalization.class.getDeclaredField("helper");
-        f.setAccessible(true);
+            net2.params().assign(p1);
 
-        assertNotNull(f.get(net1.getLayer(1)));
-        assertTrue(f.get(net1.getLayer(1)) instanceof CudnnBatchNormalizationHelper);
+            Field f = org.deeplearning4j.nn.layers.normalization.BatchNormalization.class.getDeclaredField("helper");
+            f.setAccessible(true);
 
-        f.set(net2.getLayer(1), null);
-        assertNull(f.get(net2.getLayer(1)));
+            assertNotNull(f.get(net1.getLayer(1)));
+            assertTrue(f.get(net1.getLayer(1)) instanceof CudnnBatchNormalizationHelper);
 
-        INDArray in = Nd4j.rand(new int[]{2, 3, 10, 10});
-        INDArray out1Train = net1.output(true, in)[0];
-        INDArray out2Train = net2.output(true, in)[0];
+            f.set(net2.getLayer(1), null);
+            assertNull(f.get(net2.getLayer(1)));
 
-        INDArray out1Test = net1.output(false, in)[0];
-        INDArray out2Test = net1.output(false, in)[0];
+            Field f2 = org.deeplearning4j.nn.layers.convolution.ConvolutionLayer.class.getDeclaredField("helper");
+            f2.setAccessible(true);
+            f2.set(net2.getLayer(0), null);
+            f2.set(net2.getLayer(2), null);
 
-        assertEquals(out1Train, out2Train);
-        assertEquals(out1Test, out2Test);
+            INDArray in = Nd4j.rand(new int[]{minibatch, 3, 10, 10});
+            INDArray out1Train = net1.output(true, in)[0];
+            INDArray out2Train = net2.output(true, in)[0];
+
+            INDArray out1Test = net1.output(false, in)[0];
+            INDArray out2Test = net1.output(false, in)[0];
+
+            assertEquals(out1Train, out2Train);
+            assertEquals(out1Test, out2Test);
+
+            for (int i = 0; i < 10; i++) {
+                INDArray input = Nd4j.rand(new int[]{minibatch, 3, 10, 10});
+                INDArray labels = Nd4j.rand(minibatch, 10);
+                net1.fit(new DataSet(input, labels));
+                net2.fit(new DataSet(input, labels));
+            }
+
+            in = Nd4j.rand(new int[]{minibatch, 3, 10, 10});
+            out1Train = net1.output(true, in)[0];
+            out2Train = net2.output(true, in)[0];
+
+            out1Test = net1.output(false, in)[0];
+            out2Test = net1.output(false, in)[0];
+
+            assertEquals(out1Train, out2Train);
+            assertEquals(out1Test, out2Test);
+        }
     }
 
 }
