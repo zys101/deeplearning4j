26f626ac9ba57d8189b874a7e324e5cad856f3de
raver119 <raver119@gmail.com>
2018-05-25 04:32:10 -0700

    still draft

---
+++ libnd4j/include/array/cuda/LaunchContext.cu
@@ -0,0 +1,21 @@
+//
+//  @author raver119@gmail.co
+//
+
+#include <array/LaunchContext.h>
+#include <cuda.h>
+#include <cuda_runtime_api.h>
+
+namespace nd4j {
+    void* LaunchContext::workspace() {
+        return reinterpret_cast<void *>(_reductionBuffer);
+    }
+
+    cudaStream_t* LaunchContext::stream() {
+        return _stream;
+    }
+
+    void LaunchContext::setStream(cudaStream_t *stream) {
+        _stream = stream;
+    }
+}
