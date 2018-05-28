f4af5f55ec27b21afb9d5c5c07978696fa34e53d
shugeo <sgazeos@gmail.com>
2018-05-28 19:57:49 +0300

    Fixed some build errors.

--- libnd4j/include/ops/cuda/LegacyOpExecutor.cu
+++ libnd4j/include/ops/cuda/LegacyOpExecutor.cu
@@ -27,6 +27,32 @@ namespace nd4j {
     }
 
     template <typename T>
+    void LegacyOpExecutor<T>::execSummaryStatsScalarOp(nd4j::LaunchContext &ctx, int opNum, NDArray<T> *x, NDArray<T> *z, std::vector<T> &extras, bool biasCorrected) {
+//        T res = NativeOpExcutioner<T>::execSummaryStatsScalar(opNum, x->getBuffer(), x->getShapeInfo(), extras.data(),  biasCorrected);
+//        z->putScalar(0, res);
+
+        Nd4jPointer extraPtrs[] = {nullptr, reinterpret_cast<Nd4jPointer>(ctx.stream()), nullptr, nullptr};
+        dim3 launchDims = {128, 1024, 2048};
+//dim3& launchDims, Nd4jPointer *extraPointers, int opNum, double *x, Nd4jLong *xShapeInfo, double *extraParams, bool biasCorrected
+        (*z)(0) = functions::summarystats::SummaryStatsReduce<T>::execSummaryStatsReduceScalar(launchDims, extraPtrs, opNum, x->specialBuffer(), x->specialShapeInfo(), extras.data(), biasCorrected);
+        cudaStreamSynchronize(*ctx.stream());
+
+    }
+
+    template <typename T>
+    void LegacyOpExecutor<T>::execSummaryStatsOp(nd4j::LaunchContext &ctx, int opNum, NDArray<T> *x, NDArray<T> *z, std::vector<int> &axis, std::vector<T> &extras, bool biasCorrected) {
+        Nd4jPointer extraPtrs[] = {nullptr, reinterpret_cast<Nd4jPointer>(ctx.stream()), nullptr, nullptr};
+        dim3 launchDims = {128, 1024, 2048};
+//dim3& launchDims, Nd4jPointer *extraPointers, int opNum, double *x, Nd4jLong *xShapeInfo, double *extraParams, bool biasCorrected
+        functions::summarystats::SummaryStatsReduce<T>::execSummaryStatsReduce(launchDims, extraPtrs, opNum, x->specialBuffer(), x->specialShapeInfo(), extras.data(), z->specialBuffer(),  z->specialShapeInfo(), biasCorrected);
+
+//        _CUDA_H void SummaryStatsReduce<float>::execSummaryStatsReduce(dim3& launchDims, Nd4jPointer *extraPointers, int opNum, float *x, Nd4jLong *xShapeInfo, float *extraParams, float *result, Nd4jLong *resultShapeInfo,bool biasCorrected) {
+          
+//        NativeOpExcutioner<T>::execSummaryStats(opNum, x->getBuffer(), x->getShapeInfo(), extras.data(), z->getBuffer(), z->getShapeInfo(), axis.data(), static_cast<int>(axis.size()), biasCorrected);
+        cudaStreamSynchronize(*ctx.stream());
+    }
+
+    template <typename T>
     void LegacyOpExecutor<T>::execReduceOp(nd4j::LaunchContext &ctx, int opNum, NDArray<T> *x, NDArray<T> *z, std::vector<int> &axis, std::vector<T> &extras) {
         shape::TAD tad(x->getShapeInfo(), axis.data(), static_cast<int>(axis.size()));
         tad.createTadOnlyShapeInfo();
