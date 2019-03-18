0e6f7ac37e5f545119034ef54018ab35aee4c7c9
Alex <alexander.stoyakin@gmail.com>
2019-03-18 13:31:36 +0200

    Deallocator service

--- nd4j/nd4j-backends/nd4j-api-parent/nd4j-api/src/main/java/org/nd4j/linalg/memory/deallocator/DeallocationService.java
+++ nd4j/nd4j-backends/nd4j-api-parent/nd4j-api/src/main/java/org/nd4j/linalg/memory/deallocator/DeallocationService.java
@@ -4,9 +4,16 @@ import org.nd4j.linalg.api.memory.MemoryWorkspace;
 import org.nd4j.linalg.executors.ExecutorServiceProvider;
 
 import java.lang.ref.ReferenceQueue;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class DeallocationService<T> {
+
+    private static final int THRESHOLD = 4;
+    private DeallocatorThread<T> threads[] = new DeallocatorThread[4];
+    private AtomicInteger next = new AtomicInteger(0);
 
     private static DeallocationService INSTANCE = new DeallocationService();
 
@@ -17,9 +24,14 @@ public class DeallocationService<T> {
     private ExecutorService executorService = ExecutorServiceProvider.getExecutorService();
 
     public void submit(ReferenceQueue<T> queue, ReferenceTracking<T> tracker) {
-        DeallocatorThread<T> deallocationThread =
+
+        if (next.incrementAndGet() >= THRESHOLD)
+            throw new IllegalStateException("No deallocator threads available");
+
+        threads[next.incrementAndGet()] =
                 new DeallocatorThread<>(0, queue, tracker);
-        executorService.submit(deallocationThread);
+        executorService.submit(threads[next.incrementAndGet()]);
+        next.incrementAndGet();
     }
 
 }
